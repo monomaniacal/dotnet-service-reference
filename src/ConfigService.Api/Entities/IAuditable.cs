0441c4be959e63705acf5b2ec38af960fc19e41e
Daniel Beniest <monomaniacal@users.noreply.github.com>
2026-09-13 16:31:10 +1200

    Revert "ci: expect format failure"
    This reverts commit 2da1df58001a57686cd8db41647687b350100f86.

--- src/ConfigService.Api/Entities/IAuditable.cs
+++ src/ConfigService.Api/Entities/IAuditable.cs
@@ -2,6 +2,6 @@ namespace ConfigService.Api.Entities;
 
 public interface IAuditable
 {
-    DateTime   CreatedAt { get; set; }
+    DateTime CreatedAt { get; set; }
     DateTime UpdatedAt { get; set; }
 }
